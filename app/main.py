feaa54fd358281083b1514368262786a640675a8
justblazez <iamivanmedia@gmail.com>
2026-04-08 03:47:14 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "bot is running"}
+
+@app.post("/webhook")
+async def telegram_webhook(request: Request):
+    data = await request.json()
+    print("TELEGRAM UPDATE:", data)
+    return {"ok": True}
